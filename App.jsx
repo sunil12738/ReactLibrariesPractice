e8170044782a6a5c682c029dc30423dbfd551737
Sunil Chaudhary <sunil12738@gmail.com>
2017-07-07 21:41:23 +0530

    Project Initial Setup

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+var App = React.createClass({
+
+  getInitialState: function() {
+    return {
+    };
+  },
+
+
+	render: function(){
+		return (
+			<div>
+			</div>
+		);
+	}
+});
+
+export default App;
