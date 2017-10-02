2e718e0b47b68774382a79b1053a5d5086500042
Sunil Chaudhary <sunilc@syncroutesoftware.com>
2017-10-02 13:41:06 +0530

    Convert JSX to JS

--- src/component/ReactScroll/ReactScroll.js
+++ src/component/ReactScroll/ReactScroll.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Body from "./Body.jsx"
-import Nav from "./Nav.jsx"
+import Body from "./Body.js"
+import Nav from "./Nav.js"
 
 class ReactScroll extends Component{
 
